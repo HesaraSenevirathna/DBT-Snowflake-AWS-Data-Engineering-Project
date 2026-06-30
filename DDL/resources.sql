99e084af413daa3893146da705d2721fc744655f
Hesara Senevirathna <hesarasenevirathna27@gmail.com>
2026-06-30 21:51:18 +0530

    Update resources.sql

--- DDL/resources.sql
+++ DDL/resources.sql
@@ -1,17 +1,21 @@
-CREATE OR REPLACE FILE FORMAT IF NOT EXISTS csv_format
+CREATE FILE FORMAT IF NOT EXISTS csv_format
   TYPE = 'CSV' 
   FIELD_DELIMITER = ','
   SKIP_HEADER = 1
   ERROR_ON_COLUMN_COUNT_MISMATCH = FALSE;
 
+SHOW FILE FORMATS;
 
 CREATE OR REPLACE STAGE snowstage
 FILE_FORMAT = csv_format
 URL='your_s3_bucket_path';
     
+SHOW STAGES;
 
 COPY INTO <your_table_name>
 FRoM @snowstage
 FILES=('your_file_name.csv')
 CREDENTIALS=(aws_key_id = 'yourkey', aws_secret_key = 'yoursecretkey');
+
+--You can get your credentials by going to aws and create an IAM user (Make sure to attach policies directly (s3fullaccess))
 
